844e915745a0250a1434921509fb8a6bfde9a51b
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-06-14 01:29:22 +0900

    Add files via upload
    4번째 수정

--- js/function.js
+++ js/function.js
@@ -3,7 +3,44 @@ $(function(){
       new WOW().init();
     });
   });//wow plugin 초기화
-  
+  //팝업창
+var getCookie = function (cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1);
+      if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+// 24시간 기준 쿠키 설정하기  
+var setCookie = function (cname, cvalue, exdays) {
+  var todayDate = new Date();
+  todayDate.setTime(todayDate.getTime() + (exdays*24*60*60*1000));    
+  var expires = "expires=" + todayDate.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+var couponClose = function(){
+  if($("input[id='check']").is(":checked") == true){
+      setCookie("close","Y",1);   //기간( ex. 1은 하루, 7은 일주일)
+  }
+  $(".popup").hide();
+}
+
+$(document).ready(function(){
+  cookiedata = document.cookie;
+  console.log(cookiedata);
+  if(cookiedata.indexOf("close=Y")<0){
+      $(".popup").show();
+  }else{
+      $(".popup").hide();
+  }
+  $(".close").click(function(){
+      couponClose();
+  });
+});
   $(function(){
     //var
     var $header = $('header');
@@ -46,8 +83,8 @@ $(function(){
   
     //tag
     $tag.on('click',function(event){
-      document.getElementById('myImageID').src="images/my_other_image.png";
       tagIdx = $(this).attr('href');
+  
       if(tagIdx=='sub.html'){
         $('#aboutme>.content1-left>a').trigger("click");
   
