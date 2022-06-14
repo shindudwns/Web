947932d8611a64a149c82ae12919abc8507ac9db
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-06-14 16:03:49 +0900

    Add files via upload
    12

--- js/function.js
+++ js/function.js
@@ -1,4 +1,33 @@
+$(function(){
+    $(window).on('load',function(){
+      new WOW().init();
+    });
+  });//wow plugin 초기화
+  //팝업창
+var getCookie = function (cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1);
+      if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+// 24시간 기준 쿠키 설정하기  
+var setCookie = function (cname, cvalue, exdays) {
+  var todayDate = new Date();
+  todayDate.setTime(todayDate.getTime() + (exdays*24*60*60*1000));    
+  var expires = "expires=" + todayDate.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
 
+var couponClose = function(){
+  if($("input[id='check']").is(":checked") == true){
+      setCookie("close","Y",1);   //기간( ex. 1은 하루, 7은 일주일)
+  }
+  $(".popup").hide();
+}
 
 $(document).ready(function(){
   cookiedata = document.cookie;
